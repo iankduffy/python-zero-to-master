39b0e01903ee9339131a2170abe55209a61be9c0
Ian Duffy <ian.duffy@shiftcommerce.com>
2020-01-17 15:49:50 +0000

    basics

---
+++ 01 - basics/basic.py
@@ -0,0 +1,30 @@
+# Fundamental Data Types
+int # number  3 
+float  # number with decimal 3.14 
+bool 
+str 
+list 
+tuple 
+set 
+dict 
+
+# Classes  - custome types you create
+
+
+# Specialized Data Types 
+# Types built from external packages 
+
+None # Nothing 
+
+
+# Math Functions 
+print(round(1.2))
+print(abs(-20))
+
+
+# Operation Precedence 
+
+# ()
+# ** 
+# * /
+# + - 
---
+++ 01 - basics/e-type-conversion.py
@@ -0,0 +1,5 @@
+birth_year = input('What years were you born? ')
+
+age = 2020 - int(birth_year)
+
+print(f'You are {age} years old')
